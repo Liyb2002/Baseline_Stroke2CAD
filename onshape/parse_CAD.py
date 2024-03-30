724f0a57f4f17360a2878c12502b7e5d7c293ee8
Yuanbo Li <yl4092@columbia.edu>
2024-03-30 18:39:43 -0400

    get the edge info

--- onshape/parse_CAD.py
+++ onshape/parse_CAD.py
@@ -9,23 +9,12 @@ def parseCAD(CAD_file_paths):
 
         file_info = {
             'file_path': file_path,
-            'entities': [],
+            'entities': data['entities'],
             'bounding_box': {},
             'sequence': []
         }
 
-        # # Access the entities
-        # entities = data['entities']
-
-        # for entity_id, entity_data in entities.items():
-        #     entity_info = {
-        #         'id': entity_id,
-        #         'name': entity_data['name'],
-        #         'type': entity_data['type']
-        #     }
-        #     file_info['entities'].append(entity_info)
-
-        # # Access the properties
+        # Access the properties
         # properties = data['properties']
         # bounding_box = properties['bounding_box']
         # max_point = bounding_box['max_point']
@@ -59,3 +48,47 @@ def operation_to_id(string):
         "fillet": 2
     }
     return operation_dict.get(string.lower(), None)
+
+def sketch_entity(entities):
+
+    entities_list = []
+
+    for entity in entities.items():
+        #this is not sketch operation
+        if 'entities' in entity[1]:
+            singele_entity_info = {
+            'entities': entity[1]['entities'],
+            }
+
+        #this is sketch operation
+        if 'profiles' in entity[1]:
+            profiles = entity[1]['profiles']
+            edges = profiles['edges']
+
+            singele_entity_info = {
+            'edges': [],
+            }
+
+
+            for edge in edges.items():
+                edge_id = edge[1]['id']
+                edge_type = edge[1]['param']['type']
+
+                singe_edge_info = {
+                'edge_id' : edge_id,
+                'edge_type' : edge_type,
+                'edge_direction': [],
+                'edge_origin' : []
+                }
+
+                if edge_type == 'Line':
+                    edge_direction = edge[1]['param']['direction']
+                    edge_origin = edge[1]['param']['origin']
+                    singe_edge_info['edge_direction'] = edge_direction
+                    singe_edge_info['edge_origin'] = edge_origin
+
+                singele_entity_info['edges'].append(singe_edge_info)
+        
+        entities_list.append(singele_entity_info)
+    
+    return entities_list
---
+++ sketch_param_transformer.py
@@ -0,0 +1,50 @@
+import torch
+from torch.utils.data import DataLoader, random_split
+import torch.optim as optim
+import torch.nn as nn
+from tqdm import tqdm 
+
+import preprocessing.preprocess
+import models.stroke_cloud_transformer
+import data_structure.stroke_class
+import onshape.parse_CAD
+import operation_transformer
+
+def sketch_param_transformer(dataset, num_epochs=3, batch_size=1, learning_rate=1e-3):
+    total_size = len(dataset)
+    train_size = int(0.8 * total_size) 
+    validation_size = total_size - train_size  
+
+    train_dataset, validation_dataset = random_split(dataset, [train_size, validation_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=preprocessing.io_utils.stroke_cloud_collate)
+    validation_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False, collate_fn=preprocessing.io_utils.stroke_cloud_collate)
+
+    # optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    # criterion = nn.CrossEntropyLoss()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    for epoch in range(num_epochs):
+        # model.train()
+        total_train_loss = 0
+
+        for batch in tqdm(train_loader):
+            CAD_Program_path, final_edges= batch
+
+            straight_strokes, curve_strokes = operation_transformer.separate_strokes(final_edges) 
+            parsed_CAD_program = onshape.parse_CAD.parseCAD(CAD_Program_path)
+            entity_info = onshape.parse_CAD.sketch_entity(parsed_CAD_program[0]['entities'])
+
+            print("first sketch", entity_info[0])
+            break
+
+
+
+
+stroke_cloud_dataset = preprocessing.preprocess.get_stroke_cloud()
+# model = models.stroke_cloud_transformer.StrokeToCADModel()
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# model.to(device)
+
+sketch_param_transformer(stroke_cloud_dataset)
