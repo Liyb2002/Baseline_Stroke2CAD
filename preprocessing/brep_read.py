286d18640cf543f039f031c4d7b2d9482b2daa81
Yuanbo Li <yl4092@columbia.edu>
2024-05-01 21:06:27 -0400

    Update brep_read.py

--- preprocessing/brep_read.py
+++ preprocessing/brep_read.py
@@ -1,14 +1,13 @@
 from OCC.Core.STEPControl import STEPControl_Reader
 from OCC.Core.TopoDS import TopoDS_Shape, topods
 from OCC.Core.TopExp import TopExp_Explorer
-from OCC.Core.TopAbs import TopAbs_FACE, TopAbs_EDGE
+from OCC.Core.TopAbs import TopAbs_FACE, TopAbs_EDGE, TopAbs_VERTEX
 from OCC.Core.BRepTools import breptools
 from OCC.Core.BRep import BRep_Tool
 from OCC.Core.GProp import GProp_GProps
 from OCC.Core.BRepGProp import brepgprop
 
 def read_step_file(filename):
-    """Reads a STEP file and returns a TopoDS_Shape."""
     step_reader = STEPControl_Reader()
     status = step_reader.ReadFile(filename)
     
@@ -20,12 +19,10 @@ def read_step_file(filename):
         raise Exception("Error reading STEP file.")
 
 def print_face_details(face):
-    """Prints the details of a single face."""
     u_min, u_max, v_min, v_max = breptools.UVBounds(face)
     print(f"UV Bounds: U Min = {u_min}, U Max = {u_max}, V Min = {v_min}, V Max = {v_max}")
 
 def print_edge_details(face):
-    """Prints details of all edges for a given face."""
     edge_explorer = TopExp_Explorer(face, TopAbs_EDGE)
     edge_id = 0
     while edge_explorer.More():
@@ -36,7 +33,21 @@ def print_edge_details(face):
         brepgprop.LinearProperties(edge, properties)
         length = properties.Mass()
         print(f"  Edge {edge_id}: Length = {length:.5f}")
+        print_vertex_details(edge)
         edge_explorer.Next()
+
+def print_vertex_details(edge):
+    vertex_explorer = TopExp_Explorer(edge, TopAbs_VERTEX)
+    vertices = []
+    vertex_id = 0
+    while vertex_explorer.More():
+        vertex = topods.Vertex(vertex_explorer.Current())
+        vertex_id += 1
+        pt = BRep_Tool.Pnt(vertex)
+        vertices.append((pt.X(), pt.Y(), pt.Z()))
+        print(f"Vertex {vertex_id}: Coordinates = ({pt.X():.5f}, {pt.Y():.5f}, {pt.Z():.5f})")
+        vertex_explorer.Next()
+    return vertices
 
 def main():
     # Path to your STEP file
@@ -54,6 +65,7 @@ def main():
         print(f"Face {face_id}:")
         print_face_details(face)
         print_edge_details(face)
+        print("-----------------------")
         face_explorer.Next()
 
 if __name__ == "__main__":
