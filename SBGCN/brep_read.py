8885408aca651ea73c56d63dd7bd42fb15036d4f
Yuanbo Li <yl4092@columbia.edu>
2024-05-03 21:17:41 -0400

    features

--- SBGCN/brep_read.py
+++ SBGCN/brep_read.py
@@ -22,34 +22,17 @@ def read_step_file(filename):
 
 def create_face_node(face):
     u_min, u_max, v_min, v_max = breptools.UVBounds(face)
-    print(f"UV Bounds: U Min = {u_min}, U Max = {u_max}, V Min = {v_min}, V Max = {v_max}")
+    return {"uv_bounds": (u_min, u_max, v_min, v_max)}
 
-def create_edge_node(face):
-    edge_explorer = TopExp_Explorer(face, TopAbs_EDGE)
-    edge_id = len(graph.nodes)
-    while edge_explorer.More():
-        edge = topods.Edge(edge_explorer.Current())
-        edge_id += 1
-        # Calculate the length of the edge using the new static method
-        properties = GProp_GProps()
-        brepgprop.LinearProperties(edge, properties)
-        length = properties.Mass()
-        print(f"  Edge {edge_id}: Length = {length:.5f}")
-        create_vertex_node(edge)
-        edge_explorer.Next()
+def create_edge_node(edge):
+    properties = GProp_GProps()
+    brepgprop.LinearProperties(edge, properties)
+    length = properties.Mass()
+    return {"length": length}
 
-def create_vertex_node(edge):
-    vertex_explorer = TopExp_Explorer(edge, TopAbs_VERTEX)
-    vertices = []
-    vertex_id = 0
-    while vertex_explorer.More():
-        vertex = topods.Vertex(vertex_explorer.Current())
-        vertex_id += 1
-        pt = BRep_Tool.Pnt(vertex)
-        vertices.append((pt.X(), pt.Y(), pt.Z()))
-        print(f"Vertex {vertex_id}: Coordinates = ({pt.X():.5f}, {pt.Y():.5f}, {pt.Z():.5f})")
-        vertex_explorer.Next()
-    return vertices
+def create_vertex_node(vertex):
+    pt = BRep_Tool.Pnt(vertex)
+    return {"coordinates": (pt.X(), pt.Y(), pt.Z())}
 
 def create_graph_from_step_file(step_path):
     shape = read_step_file(step_path)
@@ -61,14 +44,14 @@ def create_graph_from_step_file(step_path):
     while face_explorer.More():
         face = topods.Face(face_explorer.Current())
         face_id = len(graph.nodes)  # Generate a unique ID for the face node
-        graph.add_node(face_id, "face")
+        graph.add_node(face_id, "face", features=create_face_node(face))
         
         # Explore edges of the face
         edge_explorer = TopExp_Explorer(face, TopAbs_EDGE)
         while edge_explorer.More():
             edge = topods.Edge(edge_explorer.Current())
             edge_id = len(graph.nodes)  # Generate a unique ID for the edge node
-            graph.add_node(edge_id, "edge")
+            graph.add_node(edge_id, "edge", features=create_edge_node(edge))
             graph.add_edge(face_id, edge_id, "has_edge")
             
             # Explore vertices of the edge
@@ -76,7 +59,7 @@ def create_graph_from_step_file(step_path):
             while vertex_explorer.More():
                 vertex = topods.Vertex(vertex_explorer.Current())
                 vertex_id = len(graph.nodes)  # Generate a unique ID for the vertex node
-                graph.add_node(vertex_id, "vertex")
+                graph.add_node(vertex_id, "vertex", features=create_vertex_node(vertex))
                 graph.add_edge(edge_id, vertex_id, "has_vertex")
                 vertex_explorer.Next()
             
